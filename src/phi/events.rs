d5d1c00ca55d0482fe2e626f1aadba3954bca648
Kevin Gori <kgori@ebi.ac.uk>
2016-02-19 15:34:04 +0000

    Traits

---
+++ src/phi/events.rs
@@ -0,0 +1,102 @@
+
+macro_rules! struct_events {
+    (
+        keyboard: { $( $k_alias:ident : $k_sdl:ident ),* },
+        else: { $( $e_alias:ident : $e_sdl:pat ),* }
+    )
+
+    => {
+        use ::sdl2::EventPump;
+
+        pub struct ImmediateEvents {
+            // For every keyboard event, we have an Option<bool>
+            // Some(true)  => Was just pressed
+            // Some(false) => Was just released
+            // None        => Nothing happening _now_
+            $( pub $k_alias: Option<bool> , )*
+            $( pub $e_alias: bool ),*
+        }
+
+        impl ImmediateEvents {
+            pub fn new() -> ImmediateEvents {
+                ImmediateEvents {
+                    // Reinitialised, nothing has happened, so set None
+                    $( $k_alias: None , )*
+                    $( $e_alias: false ),*
+                }
+            }
+        }
+
+        pub struct Events {
+            pump: EventPump,
+            pub now: ImmediateEvents,
+
+            // true  => pressed
+            // false => not pressed
+            $( pub $k_alias: bool ),*
+        }
+
+
+        impl Events {
+            pub fn new(pump: EventPump) -> Events {
+                Events {
+                    pump: pump,
+                    now: ImmediateEvents::new(),
+
+                    // Default init of every key to not pressed
+                    $( $k_alias: false ),*
+                }
+            }
+
+            /// Update the events record.
+            pub fn pump(&mut self) {
+                self.now = ImmediateEvents::new();
+
+                for event in self.pump.poll_iter() {
+                    use ::sdl2::event::Event::*;
+                    use ::sdl2::keyboard::Keycode::*;
+
+                    match event {
+                        KeyDown { keycode, .. } => match keycode {
+                            // $( ... ),* containing $k_sdl and $k_alias means:
+                            //   "for every element ($k_alias : $k_sdl) pair,
+                            //    check whether the keycode is Some($k_sdl). If
+                            //    it is, then set the $k_alias fields to true."
+                            $(
+                                Some($k_sdl) => {
+                                    // Prevent holding a key being interpreted as
+                                    // multiple presses
+                                    if !self.$k_alias {
+                                        // Key pressed
+                                        self.now.$k_alias = Some(true);
+                                    }
+
+                                    self.$k_alias = true;
+                                }
+                            ),*
+                            _ => {}
+                        },
+
+                        KeyUp { keycode, .. } => match keycode {
+                            $(
+                                Some($k_sdl) => {
+                                    // Key released
+                                    self.now.$k_alias = Some(false);
+                                    self.$k_alias = false;
+                                }
+                            ),*
+                            _ => {}
+                        },
+
+                        $(
+                            $e_sdl => {
+                                self.now.$e_alias = true;
+                            }
+                        )*,
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+}
